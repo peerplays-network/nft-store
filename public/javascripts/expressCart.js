81b35ecf60a5118979d8e546ac09626335cf5fad
Dipak Savaliya <dsavaliya@peerplays.com>
2021-07-09 14:01:55 +0000

    Ds/issue165

--- public/javascripts/expressCart.js
+++ public/javascripts/expressCart.js
@@ -561,7 +561,7 @@ $(document).ready(function (){
             $('#minFundsRequired').val(minFundsRequired);
             $('#amountToAdd').val(minFundsRequired);
             $('#addFundsModal').modal('show');
-        } else {
+        }else {
             $("#buttonMint").attr("disabled", true);
             $('#nftMintModal').modal('hide');
             $('#loder').show();
